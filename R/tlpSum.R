5422748175a457fe6aedd8a5c5c7ebe4b1774c3b
Jack <pattee.jack@gmail.com>
2017-06-18 23:12:23 -0500

    pushing changes

---
+++ R/tlpSum.R
@@ -0,0 +1,105 @@
+#' @title Function to obtain TLP effect size estimates given summary statistics and a reference panel
+#' 
+#' @details Function finds the minimum of the TLP objective function 
+#' 
+#' @param cor A vector of correlations
+#' @param bfile The stem of a PLINK binary file
+#' @param lambdas Vector of values of lambda
+#' @param taus Vector of values of tau
+#' @param s Vector of values for s
+#' @param thr Convergence threshold
+#' @param init Initial values for \eqn{\beta}
+#' @param maxIter Maximum number of iterations
+#' @param extract Vector of the indices of SNPs to keep. If null, will keep all SNPs.
+#' @export
+
+tlpSum <- function(cor, bfile, lambdas, taus, s=0.5, thr=1e-4, init=NULL, maxIter=1000, extract=NULL){
+
+  fam=read.table(paste0(bfile,".fam"))
+  N=nrow(fam)
+
+  bim=read.table(paste0(bfile,".bim"))
+  P=nrow(bim)
+
+  genoMat=genotypeMatrix(paste0(bfile,".bed"),N=N,P=P,integer(0),integer(0),integer(0),integer(0),1)
+
+  sds=normalize2(genoMat)
+  genoMat[is.na(genoMat)]=0
+
+  lassoBetasFull=NULL
+  yhatFull=NULL
+  converged=NULL
+  lambdas=lambdas
+  taus=taus
+  maxIter=maxIter
+  s=s
+
+  if(is.null(extract)) extract=c(1:P)
+  
+  if(sum(extract!=FALSE)!=P) P=P-sum(extract==FALSE)
+  
+  if(is.null(init)) init = rep(0,P)
+  
+  genoMat=genoMat[,extract]
+  
+  sMult=rep(0, length(s))
+  for(i in 1:length(s)){
+    if(i==1) sMult[i] = 1-s[i]
+    else sMult[i] = (1-s[i])/(1-s[i-1])
+  }
+
+  betas=cor
+
+  for(k in 1:length(s)){
+    genoMat=genoMat*sqrt(sMult[k])
+    diagVec=rep(1-s[k],ncol(genoMat))
+    diagVec[sds==0]=0
+    denom=diagVec+s[k]
+    for(i in 1:length(lambdas)){
+      for(l in 1:length(taus)){
+        maxDiff=Inf
+        iterator=0
+        lambda=lambdas[i]
+        tau=taus[l]
+        lassoBetas=init ### RECENT CHANGE, used to start estimates at univariate correlations
+        yhat=rep(0,nrow(genoMat))
+        while(maxDiff>1e-4 & iterator<maxIter){
+          iterator=iterator+1
+          lassoBetasOld=lassoBetas
+          lambdaWts=abs(lassoBetas)<=tau
+          for(j in c(1:length(lassoBetas))){
+            lambdaTmp=lambda*lambdaWts[j]
+            if(iterator==1) lambdaTmp=lambda
+            xj=lassoBetas[j]
+            lassoBetas[j]=0
+            tempEst=diagVec[j]*xj + betas[j] - sum(genoMat[,j]*yhat)
+            if(sds[j]==0) tempEst=0
+            if(abs(tempEst)>lambdaTmp){
+              tempSign=sign(tempEst)
+              tempVal=abs(tempEst)-lambdaTmp
+              temp2=tempVal*tempSign
+              lassoBetas[j]=temp2
+            }
+            if(lassoBetas[j]!=xj){
+              del=lassoBetas[j]-xj
+              yhat=yhat+(del*genoMat[,j])
+            }
+          }
+          maxDiff=max(abs(lassoBetasOld-lassoBetas))
+        }
+        converged=rbind(converged,c(lambda,taus[l],s[k],iterator!=maxIter))
+        lassoBetasFull=cbind(lassoBetasFull,c(lambda,tau,s[k],lassoBetas))
+        yhatFull=cbind(yhatFull,c(lambda,tau,s[k],yhat))
+      }
+    }
+  }
+  toReturn=structure(list(lambdas=lambdas,taus=taus,beta=lassoBetasFull[4:nrow(lassoBetasFull),],converged=converged,pred=yhatFull[4:nrow(yhatFull),]))
+  return(toReturn)
+  #' @return a list with the following
+  #' \item{lambdas} Same as lambdas input
+  #' \item{taus} Same as taus input
+  #' \item{beta} Matrix of estimated coefficients
+  #' \item{converged} Matrix of convergence indicators with following format: column 1 is lambda value, column 2 is tau value, column 3 is s value, column 4 is 1 if there was convergence
+  #' \item{pred} Matrix of predicted phenotypes
+}
+
---
+++ src/functions.cpp
@@ -0,0 +1,229 @@
+#include <stdio.h>
+#include <string>
+#include <bitset>
+#include <fstream>
+#include <algorithm>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <RcppArmadillo.h>
+// [[Rcpp:depends(RcppArmadillo)]]
+using namespace Rcpp;
+
+/**
+Opens a Plink binary files
+@s file name
+@BIT ifstream
+@return is plink file in major mode
+*/
+bool openPlinkBinaryFile(const std::string s, std::ifstream &BIT) {
+  BIT.open(s.c_str(), std::ios::in | std::ios::binary);
+  if (!BIT.is_open()) {
+    throw "Cannot open the bed file";
+  }
+  
+  // 2) else check for 0.99 SNP/Ind coding
+  // 3) else print warning that file is too old
+  char ch[1];
+  BIT.read(ch, 1);
+  std::bitset<8> b;
+  b = ch[0];
+  bool bfile_SNP_major = false;
+  bool v1_bfile = true;
+  // If v1.00 file format
+  // Magic numbers for .bed file: 00110110 11011000 = v1.00 bed file
+  // std::cerr << "check magic number" << std::endl;
+  if ((b[2] && b[3] && b[5] && b[6]) && !(b[0] || b[1] || b[4] || b[7])) {
+    // Next number
+    BIT.read(ch, 1);
+    b = ch[0];
+    if ((b[0] && b[1] && b[3] && b[4]) && !(b[2] || b[5] || b[6] || b[7])) {
+      // Read SNP/Ind major coding
+      BIT.read(ch, 1);
+      b = ch[0];
+      if (b[0])
+        bfile_SNP_major = true;
+      else
+        bfile_SNP_major = false;
+      
+      // if (bfile_SNP_major) std::cerr << "Detected that binary PED file is
+      // v1.00 SNP-major mode" << std::endl;
+      // else std::cerr << "Detected that binary PED file is v1.00
+      // individual-major mode" << std::endl;
+      
+    } else
+      v1_bfile = false;
+    
+  } else
+    v1_bfile = false;
+  // Reset file if < v1
+  if (!v1_bfile) {
+    Rcerr << "Warning, old BED file <v1.00 : will try to recover..."
+          << std::endl;
+    Rcerr << "  but you should --make-bed from PED )" << std::endl;
+    BIT.close();
+    BIT.clear();
+    BIT.open(s.c_str(), std::ios::in | std::ios::binary);
+    BIT.read(ch, 1);
+    b = ch[0];
+  }
+  // If 0.99 file format
+  if ((!v1_bfile) && (b[1] || b[2] || b[3] || b[4] || b[5] || b[6] || b[7])) {
+    Rcerr << std::endl
+          << " *** Possible problem: guessing that BED is < v0.99      *** "
+          << std::endl;
+    Rcerr << " *** High chance of data corruption, spurious results    *** "
+          << std::endl;
+    Rcerr
+      << " *** Unless you are _sure_ this really is an old BED file *** "
+      << std::endl;
+    Rcerr << " *** you should recreate PED -> BED                      *** "
+          << std::endl
+          << std::endl;
+    bfile_SNP_major = false;
+    BIT.close();
+    BIT.clear();
+    BIT.open(s.c_str(), std::ios::in | std::ios::binary);
+  } else if (!v1_bfile) {
+    if (b[0])
+      bfile_SNP_major = true;
+    else
+      bfile_SNP_major = false;
+    Rcerr << "Binary PED file is v0.99" << std::endl;
+    if (bfile_SNP_major)
+      Rcerr << "Detected that binary PED file is in SNP-major mode"
+            << std::endl;
+      else
+        Rcerr << "Detected that binary PED file is in individual-major mode"
+              << std::endl;
+  }
+  return bfile_SNP_major;
+}
+//' imports genotypeMatrix
+//' 
+//' @param fileName location of plink binary file
+//' @param N number of subjects
+//' @param P number of SNPs
+//' @param col_skip_pos which variants to skip
+//' @param col_skip which variants to skip
+//' @param keepbytes which bytes to keep
+//' @param keepoffset the offset
+//' @param fillmissing whether to fillin missing cells
+//' @return RcppArmadillo genotype matrix
+//' 
+// [[Rcpp::export]]
+arma::mat genotypeMatrix(const std::string fileName, int N, int P,
+                         arma::Col<int> col_skip_pos, arma::Col<int> col_skip, 
+                         arma::Col<int> keepbytes, arma::Col<int> keepoffset, 
+                         const int fillmissing) {
+  
+  std::ifstream bedFile;
+  bool snpMajor = openPlinkBinaryFile(fileName, bedFile);
+  
+  if (!snpMajor)
+    throw std::runtime_error("We currently have no plans of implementing the "
+                               "individual-major mode. Please use the snp-major "
+                               "format");
+  
+  int i = 0;
+  int ii = 0;
+  const bool colskip = (col_skip_pos.n_elem > 0);
+  int Nbytes = ceil(N / 4.0);
+  const bool selectrow = (keepbytes.n_elem > 0);
+  int n, p, nskip;
+  if (selectrow)
+    n = keepbytes.n_elem;
+  else
+    n = N;
+  
+  if (colskip) {
+    nskip = arma::accu(col_skip);
+    p = P - nskip;
+  }  else
+    p = P;
+  
+  int j, jj, iii;
+  
+  arma::mat genotypes = arma::mat(n, p, arma::fill::zeros);
+  std::bitset<8> b; // Initiate the bit array
+  char ch[Nbytes];
+  
+  iii=0;
+  while (i < P) {
+    // Rcout << i << std::endl;
+    Rcpp::checkUserInterrupt();
+    if (colskip) {
+      if (ii < col_skip.n_elem) {
+        if (i == col_skip_pos[ii]) {
+          bedFile.seekg(col_skip[ii] * Nbytes, bedFile.cur);
+          i = i + col_skip[ii];
+          ii++;
+          continue;
+        }
+      }
+    }
+    
+    bedFile.read(ch, Nbytes); // Read the information
+    if (!bedFile)
+      throw std::runtime_error(
+          "Problem with the BED file...has the FAM/BIM file been changed?");
+    
+    j = 0; 
+    if (!selectrow) {
+      for (jj = 0; jj < Nbytes; jj++) {
+        b = ch[jj];
+        
+        int c = 0;
+        while (c < 7 &&
+               j < N) { // from the original PLINK: 7 because of 8 bits
+          int first = b[c++];
+          int second = b[c++];
+          if (first == 0) {
+            genotypes(j, iii) = (2 - second);
+          }
+          if(fillmissing == 0 && first == 1 && second == 0) genotypes(j, iii) =arma::datum::nan;
+          j++;
+        }
+      }
+    } else {
+      for (jj = 0; jj < keepbytes.n_elem; jj++) {
+        b = ch[keepbytes[jj]];
+        
+        int c = keepoffset[jj];
+        int first = b[c++];
+        int second = b[c];
+        if (first == 0) {
+          genotypes(j, iii) = (2 - second);
+        }
+        if(fillmissing == 0 && first == 1 && second == 0) genotypes(j, iii) =arma::datum::nan;
+        j++;
+      }
+    }
+    i++;
+    iii++; 
+  }
+  return genotypes;
+}
+
+//'normalize genotype matrix
+//'
+//'@param genotypes RcppArmadillo genotype matrix
+//'@return vector of standard deviations
+//'
+// [[Rcpp::export]]
+arma::vec normalize2(arma::mat &genotypes)
+{
+  int k = genotypes.n_cols;
+  int n = genotypes.n_rows;
+  arma::vec sd(k);
+  for (int i = 0; i < k; ++i) {
+    double m = arma::mean(genotypes.col(i));
+    arma::vec mm(n); mm.fill(m);
+    sd(i) = arma::stddev(genotypes.col(i));
+    // sd(i) = 1.0;
+    arma::vec tempVec(n); tempVec.fill(m);
+    tempVec = genotypes.col(i)-mm;
+    genotypes.col(i) = arma::normalise(tempVec);
+  }
+  return sd;
+}
